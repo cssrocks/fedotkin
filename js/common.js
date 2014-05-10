61b390cc889fcff9566bce063439ec348b1da9a7
Denis Pismenny <denis.pismenny@gmail.com>
2014-05-10 13:05:56 +0300

    add video

--- js/common.js
+++ js/common.js
@@ -1,3 +1,29 @@
 head.ready(function() {
 	console.log("These aren't the droids you're looking for!");
+	// YouTube API - doesn't work
+	var embed = 0;
+	function addEmbeded(object, videoId, width, height, autoplay) {
+		if (autoplay == 'true') {
+			object.html("<iframe width='"+width+"' height='"+height+"' src='http://www.youtube.com/embed/"+videoId + "?autoplay=1" +"' frameborder='0' allowfullscreen></iframe>");
+		} else {
+			object.html("<iframe width='"+width+"' height='"+height+"' src='http://www.youtube.com/embed/"+videoId + "?autoplay=0" +"' frameborder='0' allowfullscreen></iframe>");
+		}
+		embed+=1;
+	}
+
+	$(".embeded").each(function() {
+		if (!$(this).attr("data-autoplay")) {
+			$(this).attr("data-autoplay", 'false');
+		}
+		addEmbeded($(this), $(this).attr("data-videoId"), $(this).attr("data-width"), $(this).attr("data-height"), $(this).attr("data-autoplay"));
+	});
+	$(".video_link").click(function() {
+		if ($(this).attr("data-videoId") && $(this).find(".embeded").size() == 0) {
+			var image = $(this).find(".image");
+			addEmbeded(image, $(this).attr("data-videoId"), image.width(), image.height(), 'true');
+			image.addClass("embeded");
+		}
+		return false;
+	});
+
 });
--- js/init.js
+++ js/init.js
@@ -1,5 +1,6 @@
 head.load(
 	"js/lib/jquery.js",
+	"https://www.youtube.com/iframe_api",
 	"js/lib/social-likes.min.js",
 	"js/lib/bootstrap.min.js",
 	"js/common.js"
