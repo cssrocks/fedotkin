3f5bc93e88baaf8c99104a0bce6f217e6ba056e5
Denis Pismenny <denis.pismenny@gmail.com>
2014-05-11 09:41:02 +0300

    add select

--- js/common.js
+++ js/common.js
@@ -161,5 +161,42 @@ $(function() {
 	$(window).scroll(function() {
 		navScroll();
 	});
+	//select
+	function select() {
+		var el = $('.js-select');
+		var el_title = el.children("span");
+		var item = el.find('li');
+		var input = el.find(".js-select-input");
+		var el_text = el.find(".js-select-text");
+		var checkbox = el.find(".checkbox");
+		var list = el.find('.js-select-drop');
+		el_title.click(function(event){
+			if ($(this).parent().hasClass('is-open')) {
+				$(this).parent().removeClass('is-open');
+			}
+			else {
+				el.removeClass('is-open');
+				$(this).parent().addClass('is-open');
+			}
+			event.stopPropagation();
+		});
+		checkbox.click(function(event){
+			event.stopPropagation();
+		});
+		item.bind("click",function(){
+			el.find('li').removeClass('is-selected');
+			$(this).addClass("is-selected");
+			var text = $(this).text();
+			var id = $(this).attr("data-id");
+			$(this).parents(".js-select").find(".js-select-text").text(text);
+			$(this).parents(".js-select").find(".js-select-input").val(id);
+		});
+	}
+	select();
+
+	//click document
+	$(document).click(function() {
+		$('.js-select').removeClass('is-open');
+	});
 
 });
