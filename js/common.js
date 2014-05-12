2cc6846debd7b0e98a341c54be26e2bb5278c5b2
Denis Pismenny <denis.pismenny@gmail.com>
2014-05-12 11:31:42 +0300

    fix

--- js/common.js
+++ js/common.js
@@ -122,7 +122,7 @@ head.ready(function() {
 		$target = $(target);
 
 		$('html, body').stop().animate({
-				'scrollTop': $target.offset().top - 50
+				'scrollTop': $target.offset().top
 		}, 500, 'swing', function () {
 				window.location.hash = target;
 		});
